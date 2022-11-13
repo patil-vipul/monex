4220dddea64253f7bdb140ae0ad7c4462c18f34f
Vipul Patil <vipulrpatil.8@gmail.com>
2022-11-13 05:37:10 -0800

    first commit

--- screens/AddTransaction.js
+++ screens/AddTransaction.js
@@ -1,4 +1,4 @@
-import { ChevronLeftIcon, Button, Text, Fab, Box, Skeleton, Heading, Pressable, Center, HStack } from "native-base";
+import { ChevronLeftIcon, Button, Select, Text,TextArea ,CheckIcon, Fab, Input, Box, Skeleton, Heading, Pressable, Center, HStack } from "native-base";
 
 export default function AddTransaction({ navigation }) {
 
@@ -9,9 +9,53 @@ export default function AddTransaction({ navigation }) {
                     <Button rounded="50" w="40px" h="40px" onPress={() => navigation.goBack()} size="md" variant="ghost">
                         <ChevronLeftIcon w="40px" h="40px"></ChevronLeftIcon>
                     </Button>
-
                     <Heading>Add transaction</Heading>
                 </HStack>
+
+                <Box mt='24px'>
+                    <Text bold fontSize="lg">Amount</Text>
+                    <Input mt='16px' size="md" variant='outline' placeholder='Amount'></Input>
+                </Box>
+
+                <Box mt='24px'>
+                    <Text bold fontSize="lg">Transaction Type</Text>
+                    <Select  mt='16px' accessibilityLabel="Transaction Type" placeholder="Transaction Type" _selectedItem={{
+                        bg: "teal.600",
+                        endIcon: <CheckIcon size="5" />
+                    }}  >
+                        <Select.Item label="Credit" value="1" />
+                        <Select.Item label="Debit" value="0" />
+                    </Select>
+                </Box>
+
+                <Box mt='24px'>
+                    <Text bold fontSize="lg">Memo</Text>
+                    <TextArea mt='16px' h={20} placeholder="e.g. Milk"  />
+                </Box>
+
+                <Box mt='24px'>
+                    <Text bold fontSize="lg">Category</Text>
+                    <Select  mt='16px' accessibilityLabel="Category" placeholder="Category" _selectedItem={{
+                        bg: "teal.600",
+                        endIcon: <CheckIcon size="5" />
+                    }}  >
+                        <Select.Item label="Food" value="0" />
+                        <Select.Item label="Investment" value="1" />
+                        <Select.Item label="Travelling" value="2" />
+                        <Select.Item label="Entertainment" value="3" />
+                    </Select>
+                </Box>
+
+                <Box mt='24px'>
+                    <Text bold fontSize="lg">Transaction Type</Text>
+                    <Select  mt='16px' accessibilityLabel="Transaction Type" placeholder="Transaction Type" _selectedItem={{
+                        bg: "teal.600",
+                        endIcon: <CheckIcon size="5" />
+                    }}  >
+                        <Select.Item label="Credit" value="1" />
+                        <Select.Item label="Debit" value="0" />
+                    </Select>
+                </Box>
 
             </Box>
 
