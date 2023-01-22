3a944a9300fe75b50a82ffdc9da8b1aabbffc838
Vipul Patil <vipulrpatil.8@gmail.com>
2023-01-22 23:25:55 +0530

    formik and validations added

--- screens/AddTransaction.js
+++ screens/AddTransaction.js
@@ -1,14 +1,15 @@
 import { ChevronLeftIcon, Button, Select, Text, TextArea, CheckIcon, Fab, Input, Box, Skeleton, Heading, Pressable, Center, HStack, AddIcon, Badge, Divider, SmallCloseIcon } from "native-base";
 import { useState } from "react";
+import { Formik, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
 export default function AddTransaction({ navigation }) {
-
-    const [amount, setAmount] = useState()
-    const handleAmountChange = text => setAmount(text);
-    const [transactionType, setTransactionType] = useState(null)
-    const [memo, setMemo] = useState(null)
-    const handleMemoChange = text => setMemo(text);
-    const [category, setCategory] = useState(null)
+    const transactionSchema = yup.object().shape({
+        amount: yup.number().required('Amount is required').positive(),
+        transactionType: yup.number().required('Select a transaction type'),
+        memo: yup.string(),
+        category: yup.number(),
+      });
     const additionalTags = ['Subscription', 'Rent', 'Gaming'];
     const [selectedAdditionalTags, setSelectedAdditionalTags] = useState([])
     function handleAdditionalTagsChange(itemValue) {
@@ -22,10 +23,17 @@ export default function AddTransaction({ navigation }) {
         setSelectedAdditionalTags(tags)
     }
 
+    function createTransaction(values){
+        console.log(values)
+        
+    }
+
+
+
     return (
         <Box bg="#fff" h="100%">
             <Box margin="32px" paddingBottom="30px">
-        
+
                 <HStack space={3} alignItems="center">
                     <Button rounded="50" w="40px" h="40px" onPress={() => navigation.goBack()} size="md" variant="ghost">
                         <ChevronLeftIcon w="40px" h="40px"></ChevronLeftIcon>
@@ -33,62 +41,93 @@ export default function AddTransaction({ navigation }) {
                     <Heading>Add transaction</Heading>
                 </HStack>
 
-                <Box mt='24px'>
-                    <Text bold fontSize="lg">Amount</Text>
-                    <Input keyboardType="numeric" mt='16px' onChangeText={handleAmountChange} size="md" variant='outline' placeholder='Amount'></Input>
-                </Box>
+                <Formik initialValues={{ amount: '',transactionType:'',memo:'', category: '' }} onSubmit={createTransaction} validationSchema={transactionSchema}>
+                    {({ handleChange, handleBlur, handleSubmit, values }) => (
+                        <Box>
+                            <Box mt='24px'>
+                                <Text bold fontSize="lg">Amount</Text>
+                                <Input keyboardType="numeric" mt='16px'
+                                    onChangeText={handleChange('amount')}
+                                    onBlur={handleBlur('amount')}
+                                    value={values.amount} 
+                                    size="md" variant='outline' placeholder='Amount'></Input>
+                                <Box color="red.500" mt='2px'>
+                                    <ErrorMessage name="amount"/>
+                                </Box>
+                                
+                            </Box>
 
-                <Box mt='24px'>
-                    <Text bold fontSize="lg">Transaction Type</Text>
-                    <Select mt='16px' accessibilityLabel="Transaction Type" placeholder="Transaction Type"
-                        onValueChange={itemValue => setTransactionType(itemValue)}>
-                        <Select.Item label="Credit" value="1" />
-                        <Select.Item label="Debit" value="0" />
-                    </Select>
-                </Box>
+                            <Box mt='24px'>
+                                <Text bold fontSize="lg">Transaction Type</Text>
+                                <Select mt='16px' accessibilityLabel="Transaction Type" placeholder="Transaction Type"
+                                    onValueChange={handleChange('transactionType')}
+                                    onBlur={handleBlur('transactionType')}
+                                    value={values.transactionType}>
+                                    <Select.Item label="Credit" value="1" />
+                                    <Select.Item label="Debit" value="0" />
+                                </Select>
+                                <Box color="red.500" mt='2px'>
+                                    <ErrorMessage name="transactionType"/>
+                                </Box>
+                            </Box>
 
-                <Box mt='24px'>
-                    <Text bold fontSize="lg">Memo</Text>
-                    <TextArea onChangeText={handleMemoChange} mt='16px' h={20} placeholder="e.g. Milk" />
-                </Box>
+                            <Box mt='24px'>
+                                <Text bold fontSize="lg">Memo</Text>
+                                <TextArea 
+                                onChangeText={handleChange('memo')}
+                                onBlur={handleBlur('memo')}
+                                value={values.memo}
+                                 mt='16px' h={20} placeholder="e.g. Milk" />
+                                <Box color="red.500" mt='2px'>
+                                    <ErrorMessage name="memo"/>
+                                </Box>
+                            </Box>
 
-                <Box mt='24px' mb="24px">
-                    <Text bold fontSize="lg">Category</Text>
-                    <Select mt='16px' accessibilityLabel="Category" placeholder="Category"
-                        onValueChange={itemValue => setCategory(itemValue)}  >
-                        <Select.Item label="Food" value="0" />
-                        <Select.Item label="Investment" value="1" />
-                        <Select.Item label="Travelling" value="2" />
-                        <Select.Item label="Entertainment" value="3" />
-                    </Select>
-                </Box>
+                            <Box mt='24px' mb="24px">
+                                <Text bold fontSize="lg">Category</Text>
+                                <Select mt='16px' accessibilityLabel="Category" placeholder="Category"
+                                    onValueChange={handleChange('category')}
+                                    onBlur={handleBlur('category')}
+                                    value={values.category} >
+                                    <Select.Item label="Food" value="0" />
+                                    <Select.Item label="Investment" value="1" />
+                                    <Select.Item label="Travelling" value="2" />
+                                    <Select.Item label="Entertainment" value="3" />
+                                </Select>
+                                <Box color="red.500" mt='2px'>
+                                    <ErrorMessage name="category"/>
+                                </Box>
+                            </Box>
 
-                <Divider></Divider>
-                <Box mt='24px'>
-                    <Text bold fontSize="lg">Additional Tags</Text>
-                    <HStack mt="10px">
-                        {
-                            selectedAdditionalTags.map(key =>
-                                <Pressable key={key} onPress={() => handleAdditionalTagBadgePress(key)}>
-                                    <Badge rounded="50" mr="2" alignSelf="center" variant="solid">
-                                        <HStack alignItems="center">
-                                            <Text color="white">{key} </Text> <SmallCloseIcon color="white" />
-                                        </HStack>
-                                    </Badge>
-                                </Pressable>
-                            )
-                        }
-                    </HStack>
-                    <Select mt='16px' accessibilityLabel="Transaction Type" placeholder="Additional Tags"
-                        onValueChange={handleAdditionalTagsChange} >
-                        {additionalTags && additionalTags.filter(x => !selectedAdditionalTags.includes(x)).map(key =>
-                            <Select.Item label={key} value={key} key={key} />
-                        )}
+                            <Divider></Divider>
+                            <Box mt='24px'>
+                                <Text bold fontSize="lg">Additional Tags</Text>
+                                <HStack mt="10px">
+                                    {
+                                        selectedAdditionalTags.map(key =>
+                                            <Pressable key={key} onPress={() => handleAdditionalTagBadgePress(key)}>
+                                                <Badge rounded="50" mr="2" alignSelf="center" variant="solid">
+                                                    <HStack alignItems="center">
+                                                        <Text color="white">{key} </Text> <SmallCloseIcon color="white" />
+                                                    </HStack>
+                                                </Badge>
+                                            </Pressable>
+                                        )
+                                    }
+                                </HStack>
+                                <Select mt='16px' accessibilityLabel="Transaction Type" placeholder="Additional Tags"
+                                    onValueChange={handleAdditionalTagsChange} >
+                                    {additionalTags && additionalTags.filter(x => !selectedAdditionalTags.includes(x)).map(key =>
+                                        <Select.Item label={key} value={key} key={key} />
+                                    )}
 
-                    </Select>
-                </Box>
+                                </Select>
+                            </Box>
 
-                <Button mt='24px' endIcon={<AddIcon />}> Add Transaction  </Button>
+                            <Button mt='24px' onPress={handleSubmit} endIcon={<AddIcon />}> Add Transaction  </Button>
+                        </Box>
+                    )}
+                </Formik>
             </Box>
         </Box >
     )
