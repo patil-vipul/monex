09bb39944952de89d599e830a29684d112401036
Vipul Patil <vipulrpatil.8@gmail.com>
2023-02-04 23:07:42 +0530

    registration updated

--- screens/Registration.js
+++ screens/Registration.js
@@ -21,18 +21,12 @@ import { ErrorMessage, Formik } from 'formik'
 
 export default function Signup({ navigation }) {
   const [show, setShow] = React.useState(false);
-  const [name, setName] = React.useState(null);
   const [email, setEmail] = React.useState(null);
-  const [phone, setPhone] = React.useState(null);
   const [password, setPassword] = React.useState(null);
-  const [passwordc, setPasswordc] = React.useState(null);
 
   let userRegistrationSchema = yup.object().shape({
-    passwordc: yup.string().required().oneOf([yup.ref('password'), null], 'Passwords must match'),
     password: yup.string().required("Please enter a password").min(6),
     email: yup.string().email().required("Email is required"),
-    phone: yup.string().required("Phonenumber is required"), // (2323)23-2323 // +91 928421450 // 02542 2343225
-    name: yup.string("Name should be string").required("Name is required"),
   });
 
 
@@ -72,146 +66,62 @@ export default function Signup({ navigation }) {
   }
 
   return (
-    <Formik onSubmit={register} initialValues={{ name: '', phone: '', email: '', password: '', passwordc: '' }} validationSchema={userRegistrationSchema}>
-      {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
-        <Box h="100%" backgroundColor="white" alignItems="center" justifyContent="center">
-          <Box>
-            <Input
-              borderColor={errors.name ? 'red.500' : 'muted.300'}
-              onChangeText={handleChange('name')}
-              onBlur={handleBlur('name')}
-              value={values.name}
-              placeholder="Name"
-              InputLeftElement={
-                <Icon
-                  as={<MaterialIcons name="person" />}
-                  size={5}
-                  ml="2"
-                  color="muted.400"
+    <Box bg="#2563EB" h="100%" display="flex" flexDirection="column">
+      <Text margin="24px" fontSize="28px" fontWeight="medium" color="#FAFAFA">Sign Up</Text>
+      <Box h="100%" bg="#FAFAFA" padding="24px" borderTopLeftRadius="25px" borderTopRightRadius="25px">
+        <Formik onSubmit={register} initialValues={{  email: '', password: ''}} validationSchema={userRegistrationSchema}>
+          {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+            <Box  h="100%">
+              <Box>
+                <Input
+                  borderColor={errors.email ? 'red.500' : 'muted.300'}
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('email')}
+                  value={values.email}
+                  placeholder="Email"
                 />
-              }
-            />
-            <Box mt="2px" fontSize="sm" color="red.400">
-              <ErrorMessage name="name" />
-            </Box>
-           
-          </Box>
+                <Box mt="2px" fontSize="sm" color="red.400">
+                  <ErrorMessage name="email" />
+                </Box>
+              </Box>
 
-          <Box mt="24px">
-            <Input
-              
-              borderColor={errors.phone ? 'red.500' : 'muted.300'}
-              onChangeText={handleChange('phone')}
-              onBlur={handleBlur('phone')}
-              value={values.phone}
-              placeholder="Phone Number"
-              InputLeftElement={
-                <Icon
-                  as={<MaterialIcons name="phone" />}
-                  size={5}
-                  ml="2"
-                  color="muted.400"
-                />
-              }
-            />
-            <Box mt="2px" fontSize="sm" color="red.400">
-            <ErrorMessage name="phone" />
-            </Box>
-          </Box>
- 
-          <Box mt="24px">
-            <Input
-              
-              borderColor={errors.email ? 'red.500' : 'muted.300'}
-              onChangeText={handleChange('email')}
-              onBlur={handleBlur('email')}
-              value={values.email}
-              InputLeftElement={
-                <Icon
-                  as={<MaterialIcons name="email" />}
-                  size={5}
-                  ml="2"
-                  color="muted.400"
-                />
-              }
-              placeholder="Email Id"
-            />
-             <Box mt="2px" fontSize="sm" color="red.400">
-            <ErrorMessage name="email" />
-            </Box>
-          </Box>
-
-          <Box mt="24px">
-            <Input
-              
-              borderColor={errors.password ? 'red.500' : 'muted.300'}
-              onChangeText={handleChange('password')}
-              onBlur={handleBlur('password')}
-              value={values.password}
-              type={show ? "text" : "password"}
-              InputRightElement={
-                <Pressable onPress={() => setShow(!show)}>
-                  <Icon
-                    as={
-                      <MaterialIcons
-                        name={show ? "visibility" : "visibility-off"}
+              <Box mt="16px">
+                <Input
+                  borderColor={errors.password ? 'red.500' : 'muted.300'}
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  value={values.password}
+                  type={show ? "text" : "password"}
+                  InputRightElement={
+                    <Pressable onPress={() => setShow(!show)}>
+                      <Icon
+                        as={
+                          <MaterialIcons
+                            name={show ? "visibility" : "visibility-off"}
+                          />
+                        }
+                        size={5}
+                        mr="2"
+                        color="muted.400"
                       />
-                    }
-                    size={5}
-                    mr="2"
-                    color="muted.400"
-                  />
-                </Pressable>
-              }
-              placeholder="Password"
-            />
-             <Box mt="2px" fontSize="sm" color="red.400">
-            <ErrorMessage name="password" />
+                    </Pressable>
+                  }
+                  placeholder="Password"
+                />
+                <Box mt="2px" fontSize="sm" color="red.400">
+                  <ErrorMessage name="password" />
+                </Box>
+              </Box>
+
+              <Button mt="24px" bg="#2563EB" onPress={handleSubmit}>Register</Button>
+
+              <Link mt="24px" onPress={() => navigation.navigate('login2')}>Login</Link>
+
             </Box>
-          </Box>
+          )}
+        </Formik>
+      </Box>
+    </Box>
 
-          <Box mt="24px">
-            <Input
-              
-              borderColor={errors.passwordc ? 'red.500' : 'muted.300'}
-              onChangeText={handleChange('passwordc')}
-              onBlur={handleBlur('passwordc')}
-              value={values.passwordc}
-
-              type={show ? "text" : "password"}
-              InputRightElement={
-                <Pressable onPress={() => setShow(!show)}>
-
-                  <Icon
-                    as={
-                      <MaterialIcons
-                        name={show ? "visibility" : "visibility-off"}
-                      />
-                    }
-                    size={5}
-                    mr="2"
-                    color="muted.400"
-                  />
-                </Pressable>
-              }
-              placeholder="Re-Enter Password"
-            />
-             <Box mt="2px" fontSize="sm" color="red.400">
-            <ErrorMessage name="passwordc" />
-            </Box>
-          </Box>
-         
-
-          <Box mt="24px" alignItems="center">
-            <Button onPress={handleSubmit}>Signup</Button>
-          </Box>
-
-          <Box mt="12px" alignItems="center">
-            <Link onPress={() => navigation.navigate('login2')}>Already have an account ? Log In</Link>
-          </Box>
-
-        </Box>
-      )}
-    </Formik>
   );
 }
