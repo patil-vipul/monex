2efdbf4c1e280ae9c5df3a0dcffd14453ac314fc
Bhavesh Yeole <byeole80@gmail.com>
2023-02-21 23:49:58 +0530

    navbar

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
+import React from "react";
 import { NativeBaseProvider } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "./screens/Home";
 import AddTransaction from "./screens/AddTransaction";
 import Login from "./screens/Login";
 import Registration from "./screens/Registration";
-import Splash from "./screens/Splash"
-import Transactions from "./screens/Transactions"
-import TransactionDetails from "./screens/TransactionDetails"
+import Splash from "./screens/Splash";
+import Transactions from "./screens/Transactions";
+import TransactionDetails from "./screens/TransactionDetails";
 import Menu from "./screens/Menu";
 
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Budget" component={Menu} />
+      <Tab.Screen name="Analytics" component={Transactions} />
+      <Tab.Screen name="Profile" component={TransactionDetails} />
+    </Tab.Navigator>
+  );
+};
+
 export default function App() {
-  const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
       <NativeBaseProvider>
@@ -19,18 +34,19 @@ export default function App() {
           initialRouteName="Splash"
           screenOptions={{ headerShown: false, cardStyle: { flex: 1 } }}
         >
-          <Stack.Screen
-            options={{ cardStyle: { flex: 1 } }}
-            name="Home"
-            component={Home}
-          />
-          <Stack.Screen name="AddTransaction" component={AddTransaction} />
+          <Stack.Screen name="Splash" component={Splash} />
           <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="Registration" component={Registration} />
-          <Stack.Screen name="Menu" component={Menu} />
-          <Stack.Screen name="Splash" component={Splash} />
-          <Stack.Screen name="Transactions" component={Transactions} />
-          <Stack.Screen name="TransactionDetails" component={TransactionDetails} />
+          <Stack.Screen
+            name="Home"
+            options={{ headerShown: false }}
+            component={Tabs}
+          />
+          <Stack.Screen
+            name="AddTransaction"
+            options={{ title: "Add Transaction" }}
+            component={AddTransaction}
+          />
         </Stack.Navigator>
       </NativeBaseProvider>
     </NavigationContainer>
---
+++ navigation/tabs.js
@@ -0,0 +1,23 @@
+// // navigation/Tabs.js
+
+// import React from "react";
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import Home from "../screens/Home";
+// import Menu from "../screens/Menu";
+// import Transactions from "../screens/Transactions";
+// import TransactionDetails from "../screens/TransactionDetails";
+
+// const Tab = createBottomTabNavigator();
+
+// const Tabs = () => {
+//   return (
+//     <Tab.Navigator>
+//       <Tab.Screen name="Home" component={Home} />
+//       <Tab.Screen name="Budget" component={Menu} />
+//       <Tab.Screen name="Analytics" component={Transactions} />
+//       <Tab.Screen name="Profile" component={TransactionDetails} />
+//     </Tab.Navigator>
+//   );
+// };
+
+// export default Tabs;
